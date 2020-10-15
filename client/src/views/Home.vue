ffdc58d79a8bbc28b80303c32696bd9d1806ebcf
JULIAN RAZIF FIGARO <julian.razif@gmail.com>
2020-10-15 18:13:03 +0700

    Refactoring vue default view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="fixed-sn white-skin animated">
+    <header>
+      <nav class="navbar fixed-top navbar-expand-lg scrolling-navbar double-nav">
+        <h3>Quiz kajool</h3>
+      </nav>
+    </header>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
